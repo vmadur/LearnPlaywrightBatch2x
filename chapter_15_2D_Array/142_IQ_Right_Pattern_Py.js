529ca5d4fc17a5baee2e7710705b5ca0cbe2ffad
LearnPlaywrightBatch2x CI <noreply@learnplaywrightbatch2x.local>
2026-06-08 12:17:29 -0400

    Add pyramid patterns to 142_IQ_Right_Pattern_Py.js

--- chapter_15_2D_Array/142_IQ_Right_Pattern_Py.js
+++ chapter_15_2D_Array/142_IQ_Right_Pattern_Py.js
@@ -10,3 +10,26 @@ for (let i = 1; i <= n; i++) {
     }
     console.log(row.trim());
 }
+// Pyramid pattern 
+
+for (let i = 1; i <= 5; i++) {
+    let row = ""
+    for (j = 1; j <= 5 - i; j++) {
+        row += " "
+    }
+    for (let k = 1; k <= (2 * i - 1); k++) {
+        row += "*"
+    }
+
+    console.log(row)
+}
+
+// Reverse right-aligned pyramid
+
+for (let i = 5; i >= 1; i--) {
+    let row = " "
+    for (j = 1; j <= i; j++) {
+        row += "*"
+    }
+    console.log(row.trim())
+}
